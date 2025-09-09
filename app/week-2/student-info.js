5ba6da86ad7c02891d031f0424d59abe76cb789d
Prince Pande <207639394+princepierce8@users.noreply.github.com>
2025-09-09 14:19:42 -0600

    Initial commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+
+export default function MyInfo(){
+
+
+    
+    return(
+        <main>
+            <h1>Name: Prince Pande</h1>
+            <ul>
+                <li><Link href="https://github.com/princepierce8/cprg306-assignment.git" ClassName="text-blue-600 hover: underline hover:">My Github</Link></li>
+            </ul>
+        </main>
+    )
+}
